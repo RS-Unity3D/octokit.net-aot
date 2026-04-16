da921c3203fb9774490410cf2f5264cfadef24b6
RainsSoft <andyhebear@163.com>
2026-04-16 10:46:16 +0800

    Update RepositoryCommentsClient.cs

--- Octokit/Clients/RepositoryCommentsClient.cs
+++ Octokit/Clients/RepositoryCommentsClient.cs
@@ -25,27 +25,27 @@ namespace Octokit
         /// </summary>
         /// <param name="owner">The owner of the repository</param>
         /// <param name="name">The name of the repository</param>
-        /// <param name="commentId">The comment id</param>
+        /// <param name="number">The comment id</param>
         /// <remarks>http://developer.github.com/v3/repos/comments/#get-a-single-commit-comment</remarks>
         [ManualRoute("GET", "/repos/{owner}/{repo}/comments/{comment_id}")]
-        public Task<CommitComment> Get(string owner, string name, long commentId)
+        public Task<CommitComment> Get(string owner, string name, int number)
         {
             Ensure.ArgumentNotNullOrEmptyString(owner, nameof(owner));
             Ensure.ArgumentNotNullOrEmptyString(name, nameof(name));
 
-            return ApiConnection.Get<CommitComment>(ApiUrls.CommitComment(owner, name, commentId), null);
+            return ApiConnection.Get<CommitComment>(ApiUrls.CommitComment(owner, name, number), null);
         }
 
         /// <summary>
         /// Gets a single Repository Comment by number.
         /// </summary>
         /// <param name="repositoryId">The Id of the repository</param>
-        /// <param name="commentId">The comment id</param>
+        /// <param name="number">The comment id</param>
         /// <remarks>http://developer.github.com/v3/repos/comments/#get-a-single-commit-comment</remarks>
         [ManualRoute("GET", "/repositories/{id}/comments/{number}")]
-        public Task<CommitComment> Get(long repositoryId, long commentId)
+        public Task<CommitComment> Get(long repositoryId, int number)
         {
-            return ApiConnection.Get<CommitComment>(ApiUrls.CommitComment(repositoryId, commentId), null);
+            return ApiConnection.Get<CommitComment>(ApiUrls.CommitComment(repositoryId, number), null);
         }
 
         /// <summary>
@@ -211,32 +211,32 @@ namespace Octokit
         /// </summary>
         /// <param name="owner">The owner of the repository</param>
         /// <param name="name">The name of the repository</param>
-        /// <param name="commentId">The comment id</param>
+        /// <param name="number">The comment number</param>
         /// <param name="commentUpdate">The modified comment</param>
         /// <remarks>http://developer.github.com/v3/repos/comments/#update-a-commit-comment</remarks>
         [ManualRoute("PATCH", "/repos/{owner}/{repo}/comments/{comment_id}")]
-        public Task<CommitComment> Update(string owner, string name, long  commentId, string commentUpdate)
+        public Task<CommitComment> Update(string owner, string name, int number, string commentUpdate)
         {
             Ensure.ArgumentNotNullOrEmptyString(owner, nameof(owner));
             Ensure.ArgumentNotNullOrEmptyString(name, nameof(name));
             Ensure.ArgumentNotNull(commentUpdate, nameof(commentUpdate));
 
-            return ApiConnection.Patch<CommitComment>(ApiUrls.CommitComment(owner, name, commentId), new BodyWrapper(commentUpdate));
+            return ApiConnection.Patch<CommitComment>(ApiUrls.CommitComment(owner, name, number), new BodyWrapper(commentUpdate));
         }
 
         /// <summary>
         /// Updates a specified Commit Comment.
         /// </summary>
         /// <param name="repositoryId">The Id of the repository</param>
-        /// <param name="commentId">The comment id</param>
+        /// <param name="number">The comment number</param>
         /// <param name="commentUpdate">The modified comment</param>
         /// <remarks>http://developer.github.com/v3/repos/comments/#update-a-commit-comment</remarks>
         [ManualRoute("PATCH", "/repositories/{id}/comments/{number}")]
-        public Task<CommitComment> Update(long repositoryId, long commentId, string commentUpdate)
+        public Task<CommitComment> Update(long repositoryId, int number, string commentUpdate)
         {
             Ensure.ArgumentNotNull(commentUpdate, nameof(commentUpdate));
 
-            return ApiConnection.Patch<CommitComment>(ApiUrls.CommitComment(repositoryId, commentId), new BodyWrapper(commentUpdate));
+            return ApiConnection.Patch<CommitComment>(ApiUrls.CommitComment(repositoryId, number), new BodyWrapper(commentUpdate));
         }
 
         /// <summary>
@@ -244,27 +244,27 @@ namespace Octokit
         /// </summary>
         /// <param name="owner">The owner of the repository</param>
         /// <param name="name">The name of the repository</param>
-        /// <param name="commentId">The comment id</param>
+        /// <param name="number">The comment id</param>
         /// <remarks>http://developer.github.com/v3/repos/comments/#delete-a-commit-comment</remarks>
         [ManualRoute("DELETE", "/repos/{owner}/{repo}/comments/{comment_id}")]
-        public Task Delete(string owner, string name, long commentId)
+        public Task Delete(string owner, string name, int number)
         {
             Ensure.ArgumentNotNullOrEmptyString(owner, nameof(owner));
             Ensure.ArgumentNotNullOrEmptyString(name, nameof(name));
 
-            return ApiConnection.Delete(ApiUrls.CommitComment(owner, name, commentId));
+            return ApiConnection.Delete(ApiUrls.CommitComment(owner, name, number));
         }
 
         /// <summary>
         /// Deletes the specified Commit Comment
         /// </summary>
         /// <param name="repositoryId">The Id of the repository</param>
-        /// <param name="commentId">The comment id</param>
+        /// <param name="number">The comment id</param>
         /// <remarks>http://developer.github.com/v3/repos/comments/#delete-a-commit-comment</remarks>
         [ManualRoute("DELETE", "/repositories/{id}/comments/{number}")]
-        public Task Delete(long repositoryId, long commentId)
+        public Task Delete(long repositoryId, int number)
         {
-            return ApiConnection.Delete(ApiUrls.CommitComment(repositoryId, commentId));
+            return ApiConnection.Delete(ApiUrls.CommitComment(repositoryId, number));
         }
     }
 }
