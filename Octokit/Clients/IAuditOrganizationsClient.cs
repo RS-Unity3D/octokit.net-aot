3d58c954ff6e32a072871882dc9774147512dc61
RainsSoft <andyhebear@163.com>
2026-04-16 10:37:14 +0800

    Create IAuditOrganizationsClient.cs

---
+++ Octokit/Clients/IAuditOrganizationsClient.cs
@@ -0,0 +1,45 @@
+#nullable enable
+using System;
+using System.Threading.Tasks;
+
+namespace Octokit
+{
+    public interface IAuditOrganizationsClient
+    {
+        /// <summary>
+        /// Gets last activity date for an organization
+        /// </summary>
+        /// <param name="organization">The organization</param>
+        Task<DateTime?> GetLastActivityDate(string organization);
+        
+        /// <summary>
+        /// Gets user last activity date for a repository
+        /// </summary>
+        /// <remarks>
+        /// http://developer.github.com/v3/activity/events/#list-issue-events-for-a-repository
+        /// </remarks>
+        /// <param name="organization">The organization</param>
+        /// <param name="auditLogPhraseOptions">The query phrase options</param>
+        Task<DateTime?> GetUserLastActivityForRepositoryDate(string organization, AuditLogPhraseOptions auditLogPhraseOptions);
+        
+        /// <summary>
+        /// Gets last visibility change event for a given repository
+        /// </summary>
+        /// <remarks>
+        /// http://developer.github.com/v3/activity/events/#list-issue-events-for-a-repository
+        /// </remarks>
+        /// <param name="organization">The organization</param>
+        /// <param name="auditLogPhraseOptions">The query phrase options</param>
+        Task<RepositoryVisibilityChangeEvent?> GetRepositoryVisibilityChangeLastEvent(string organization, AuditLogPhraseOptions auditLogPhraseOptions);
+
+        /// <summary>
+        /// Gets last visibility change event for a given repository
+        /// </summary>
+        /// <remarks>
+        /// http://developer.github.com/v3/activity/events/#list-issue-events-for-a-repository
+        /// </remarks>
+        /// <param name="organization">The organization</param>
+        /// <param name="auditLogPhraseOptions">The query phrase options</param>
+        Task<RepositoryCreatedEvent?> GetRepositoryCreatedLastEvent(string organization, AuditLogPhraseOptions auditLogPhraseOptions);
+    }
+}
